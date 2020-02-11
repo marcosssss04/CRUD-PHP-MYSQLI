dfd463078e5d17c92f5c194d15218b83c80429f6
marcos aurelio <marcosssss1991@gmail.com>
2020-02-11 19:34:52 -0300

    first commit

---
+++ includes/message.php
@@ -0,0 +1,15 @@
+<?php
+// Sessao 
+session_start();
+if(isset($_SESSION['mensagem'])): ?>
+<script>
+   // Mensagem
+    window.onload = function() {
+        M.toast({html: '<?php echo $_SESSION['mensagem']; ?>'});
+    };
+</script>
+
+<?php
+endif;
+session_unset();
+?>
---
+++ php_action/create.php
@@ -0,0 +1,31 @@
+<?php
+// Sessao
+session_start();
+// conexao
+require_once 'db_connect.php';
+// Clear 
+function clear($input){
+    global $connect;
+    // sql
+    $var = mysqli_escape_string($connect, $input);
+    // xss
+    $var = htmlspecialchars($var);
+    return $var;
+}
+
+if(isset($_POST['btn-cadastrar'])):
+    $nome = clear($_POST['nome']);
+    $sobrenome = clear($connect, $_POST['sobrenome']);
+    $email = clear($connect, $_POST['email']);
+    $idade = clear($connect, $_POST['idade']);
+
+    $sql = "INSERT INTO clientes (nome, sobrenome, email, idade) VALUES ('$nome', '$sobrenome', '$email', '$idade')";
+
+    if(mysqli_query($connect, $sql)):
+        $_SESSION['mensagem'] = "Cadastrado com sucesso!";
+        header('Location: ../index.php');
+    else:
+        $_SESSION['mensagem'] = "Erro ao cadastrar";
+        header('Location: ../index.php');  
+    endif;     
+endif;    
